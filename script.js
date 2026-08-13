c957c3a99d590549974cbbce6a93b735463b8153
Mangesh Lolage <lolagemangesh2003@gmail.com>
2026-08-13 14:02:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function updateTimer() {
+  const now = new Date();
+  const dateTimeString = now.toLocaleString();
+  document.getElementById("timer").textContent = dateTimeString;
+}
+
+updateTimer();
+setInterval(updateTimer, 1000);
